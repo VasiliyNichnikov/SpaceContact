e716169c035fdd3869590afc3c379fb1ae729437
vnich <v.nichnikov@gmail.com>
2026-02-04 12:03:00 +0300

    Added SimpleConnectionDialog for testing connection

---
+++ Assets/Scripts/Client/Dialogs/SimpleConnectionDialog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+using Logger = Logs.Logger;
+
+namespace Client.Dialogs
+{
+    public class SimpleConnectionDialog : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _hostButton = null!;
+        [SerializeField]
+        private Button _clientButton = null!;
+        
+        [SerializeField]
+        private NetworkManager _networkManager = null!;
+
+        private void Start()
+        {
+            _hostButton.onClick.AddListener(() =>
+            {
+                Logger.Log("Host button clicked.");
+                _networkManager.StartHost();
+                HideUi();
+            });
+            
+            _clientButton.onClick.AddListener(() =>
+            {
+                Logger.Log("Client Button clicked.");
+                _networkManager.StartClient();
+                HideUi();
+            });
+        }
+
+        private void HideUi()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
