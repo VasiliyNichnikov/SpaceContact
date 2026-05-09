aaf6cbfb0134ff3557dbd202ef75a3b68b373e40
vasiliy <v.nichnikov@gmail.com>
2026-05-09 15:00:26 +0400

    Fix serializtion error

--- Assets/Scripts/Network/Infrastructure/NewtonsoftSerializer.cs
+++ Assets/Scripts/Network/Infrastructure/NewtonsoftSerializer.cs
@@ -5,12 +5,14 @@ namespace Network.Infrastructure
 {
     public class NewtonsoftSerializer : INetworkSerializer
     {
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto
+        };
+
         public byte[] Serialize<T>(T data)
         {
-            var settings = new JsonSerializerSettings {
-                TypeNameHandling = TypeNameHandling.Auto
-            };
-            var json = JsonConvert.SerializeObject(data, settings);
+            var json = JsonConvert.SerializeObject(data, _settings);
             
             return System.Text.Encoding.UTF8.GetBytes(json);
         }
@@ -18,15 +20,15 @@ namespace Network.Infrastructure
         public T Deserialize<T>(byte[] data)
         {
             var json = System.Text.Encoding.UTF8.GetString(data);
-            
-            return JsonConvert.DeserializeObject<T>(json) ?? throw new InvalidOperationException();
+
+            return JsonConvert.DeserializeObject<T>(json, _settings) ?? throw new InvalidOperationException();
         }
 
         public object Deserialize(Type type, byte[] data)
         {
             var json = System.Text.Encoding.UTF8.GetString(data);
-            
-            return JsonConvert.DeserializeObject(json, type) ?? throw new InvalidOperationException();
+
+            return JsonConvert.DeserializeObject(json, type, _settings) ?? throw new InvalidOperationException();
         }
     }
 }
